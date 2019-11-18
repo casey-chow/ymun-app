aff640c767dbd8c1a59da49b548d4299731d2529
Casey Chow <casey.chow@gmail.com>
2019-11-17 22:59:04 -0800

    enable suspense and error boundary for rest-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,13 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs,
+  IonSpinner,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { apps, flash, send } from 'ionicons/icons';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { CacheProvider } from 'rest-hooks';
+import { CacheProvider, NetworkErrorBoundary } from 'rest-hooks';
 
 import Details from './pages/Details';
 import Tab1 from './pages/Tab1';
@@ -37,35 +38,39 @@ import Tab3 from './pages/Tab3';
 const App: React.FC = () => (
   <CacheProvider>
     <IonApp>
-      <IonReactRouter>
-        <IonTabs>
-          <IonRouterOutlet>
-            <Route path="/tab1" component={Tab1} exact={true} />
-            <Route path="/tab2" component={Tab2} exact={true} />
-            <Route path="/tab2/details" component={Details} />
-            <Route path="/tab3" component={Tab3} />
-            <Route
-              path="/"
-              render={() => <Redirect to="/tab1" />}
-              exact={true}
-            />
-          </IonRouterOutlet>
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="tab1" href="/tab1">
-              <IonIcon icon={flash} />
-              <IonLabel>Tab One</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab2" href="/tab2">
-              <IonIcon icon={apps} />
-              <IonLabel>Tab Two</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="tab3" href="/tab3">
-              <IonIcon icon={send} />
-              <IonLabel>Tab Three</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </IonReactRouter>
+      <Suspense fallback={<IonSpinner />}>
+        <NetworkErrorBoundary>
+          <IonReactRouter>
+            <IonTabs>
+              <IonRouterOutlet>
+                <Route path="/tab1" component={Tab1} exact={true} />
+                <Route path="/tab2" component={Tab2} exact={true} />
+                <Route path="/tab2/details" component={Details} />
+                <Route path="/tab3" component={Tab3} />
+                <Route
+                  path="/"
+                  render={() => <Redirect to="/tab1" />}
+                  exact={true}
+                />
+              </IonRouterOutlet>
+              <IonTabBar slot="bottom">
+                <IonTabButton tab="tab1" href="/tab1">
+                  <IonIcon icon={flash} />
+                  <IonLabel>Tab One</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="tab2" href="/tab2">
+                  <IonIcon icon={apps} />
+                  <IonLabel>Tab Two</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="tab3" href="/tab3">
+                  <IonIcon icon={send} />
+                  <IonLabel>Tab Three</IonLabel>
+                </IonTabButton>
+              </IonTabBar>
+            </IonTabs>
+          </IonReactRouter>
+        </NetworkErrorBoundary>
+      </Suspense>
     </IonApp>
   </CacheProvider>
 );
