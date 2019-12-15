921cc3ff71e2f83cd4e8f68c000467be3a94df69
Casey Chow <casey.chow@gmail.com>
2019-12-15 14:19:39 -0800

    wip: expand instead

--- src/pages/EventList/EventListItem.tsx
+++ src/pages/EventList/EventListItem.tsx
@@ -1,48 +1,73 @@
-import { IonItem, IonLabel, IonText } from '@ionic/react';
+import { IonIcon, IonItem, IonLabel, IonText } from '@ionic/react';
 import dayjs from 'dayjs';
+import { arrowDropleft, arrowDropdown } from 'ionicons/icons';
 import { isNil } from 'lodash';
-import * as React from 'react';
+import React, { useCallback } from 'react';
 import { useCache } from 'rest-hooks';
 import EventResource from '../../resources/event';
 import LocationResource from '../../resources/location';
 
 interface EventDetailProps {
   readonly event: EventResource;
+  readonly expanded: boolean;
+  readonly onClick: (expanded: boolean) => void;
 }
 
 const timeFormat = 'h:mm A';
 
-const EventsListItem: React.FC<EventDetailProps> = ({ event }) => {
+const EventsListItem: React.FC<EventDetailProps> = ({
+  event,
+  expanded,
+  onClick,
+}) => {
   const location = useCache(
     LocationResource.detailShape(),
     isNil(event.location) ? null : { id: event.location }
   );
 
+  const memoizedOnClick = useCallback(() => {
+    onClick(expanded);
+  }, [expanded, onClick]);
+
   return (
-    <IonItem routerLink={`/events/${event.id}`}>
+    <IonItem
+      style={{
+        cursor: 'pointer',
+      }}
+      onClick={memoizedOnClick}
+      className="ion-activatable"
+    >
       <div slot="start" className="ion-text-end">
-        <IonLabel>
-          <small>
-            <strong>{dayjs(event.start_time).format(timeFormat)}</strong>
-            <br />
-            <IonText color="medium">
-              {dayjs(event.end_time).format(timeFormat)}
-            </IonText>
-          </small>
-        </IonLabel>
+        <small>
+          <strong>{dayjs(event.start_time).format(timeFormat)}</strong>
+          <br />
+          <IonText color="medium">
+            {dayjs(event.end_time).format(timeFormat)}
+          </IonText>
+        </small>
       </div>
 
       <IonLabel>
-        {event.title}
+        <span>{event.title}</span>
         {location && (
-          <>
-            <br />
+          <p>
             <IonText color="medium">
               <small>{location.name}</small>
             </IonText>
-          </>
+          </p>
+        )}
+        {expanded && (
+          <div dangerouslySetInnerHTML={{ __html: event.description }} />
         )}
       </IonLabel>
+
+      <div slot="end" className="ion-text-end">
+        {expanded ? (
+          <IonIcon icon={arrowDropdown} />
+        ) : (
+          <IonIcon icon={arrowDropleft} />
+        )}
+      </div>
     </IonItem>
   );
 };
