a337faf70a3a806b093244304d3fe6f9b017f275
Casey Chow <casey.chow@gmail.com>
2019-12-15 16:15:45 -0800

    update endpoint

--- src/resources/event.ts
+++ src/resources/event.ts
@@ -1,7 +1,7 @@
 import BaseResource from './base';
 
 export default class EventResource extends BaseResource {
-  static urlRoot = 'https://ymun-app-api.caseychow.org/_/items/events';
+  static urlRoot = 'https://ymun-app.caseychow.org/_/items/events';
 
   readonly status?: string;
 
