7565ae80a4fc87b5c6a3d749be82f15473069d75
hanskamin <hansjkamin@gmail.com>
2019-12-17 01:50:22 -0600

    created photo slider for home page

--- src/pages/Home/PhotoSlider.tsx
+++ src/pages/Home/PhotoSlider.tsx
@@ -1,114 +1,70 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { IonSlides, IonSlide, IonImg, IonButton } from '@ionic/react';
 
-type SlideProps = {
-  image: string;
-  key: number;
+// Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+const slideOpts = {
+  initialSlide: 0,
+  speed: 400,
+  spaceBetween: 0,
+  autoplay: true,
+  loop: true,
 };
 
-type ArrowProps = {
-  previousSlide?: () => void;
-  nextSlide?: () => void;
+const styles = {
+  image: {
+    width: '100vw',
+    height: '50vh',
+    objectFit: 'cover',
+    position: 'relative',
+  },
+  button: {
+    width: 'auto',
+    height: '25px',
+    position: 'absolute',
+    zIndex: '99',
+    bottom: '10px',
+    right: '10px',
+  },
+};
+
+/**
+ * Generates and returns an image slide.
+ * @param image the location of the image in our assets.
+ * @param key the index of the image in our images list.
+ * @returns an IonSlide containing an IonImg.
+ */
+const createSlide = (image: string, key: number): JSX.Element => {
+  return (
+    <IonSlide key={key}>
+      <IonImg key={key} src={image} style={styles.image} />
+      <IonButton
+        color={'primary'}
+        size={'small'}
+        style={styles.button}
+        href={'/posts'}
+      >
+        GALLERY
+      </IonButton>
+    </IonSlide>
+  );
 };
 
 const PhotoSlider: React.FC = () => {
   // TODO: retrieve images from our backend.
-  // eslint-disable-next-line
-  const [images, setImages] = useState(['/assets/liberators.jpg', '/assets/daboys.jpg']);
-  const [index, setIndex] = useState(0);
-  const [movement, setMovement] = useState(0);
-
-  /**
-   * Finds the photo slider and retrieves its width.
-   * @returns the width of the photo slider if found, 0 otherwise.
-   */
-  const slideWidth = (): number => {
-    const slide: Element | null = document.querySelector('.slide');
-    return slide ? slide.clientWidth : 0;
-  };
-
-  /**
-   * Moves the slider to the previous photo.
-   */
-  const previousSlide = (): void => {
-    if (index !== 0) {
-      setIndex(index - 1);
-      setMovement(movement + slideWidth());
-    }
-  };
-
-  /**
-   * Moves the slider to the next photo.
-   */
-  const nextSlide = (): void => {
-    // Move to the beginning of the array when we're at the end.
-    if (index === images.length - 1) {
-      setIndex(0);
-      setMovement(0);
-    } else {
-      setIndex(index + 1);
-      setMovement(movement - slideWidth());
-    }
-  };
+  const images = [
+    '/assets/2015.jpg',
+    '/assets/turnup.jpg',
+    '/assets/caucus.jpg',
+    '/assets/liberators.jpg',
+    '/assets/daboys.jpg',
+  ];
 
   return (
-    <div className="slider">
-      <div
-        className="slider-wrapper"
-        style={{
-          transform: `translateX(${movement}px)`,
-          transition: 'transform ease-out 0.45s',
-        }}
-      >
-        {images.map((image, i) => (
-          <Slide image={image} key={i} />
-        ))}
-      </div>
-      <LeftArrow previousSlide={previousSlide} />
-      <RightArrow nextSlide={nextSlide} />
-    </div>
-  );
-};
-
-/**
- * Represents the current slide.
- * @param props.image the file path of the image.
- * @param props.key the index (id) of the image.
- * @returns JSX element representing a slide of an image.
- */
-const Slide = (props: SlideProps): JSX.Element => {
-  const styles = {
-    backgroundImage: props.image,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: '50% 60%',
-  };
-
-  return <div className="slide" style={styles} />;
-};
-
-/**
- * Represents the left arrow overlayed on the current slide.
- * @param props.previousSlide the function to get the previous slide.
- * @returns JSX element representing a clickable left arrow.
- */
-const LeftArrow = (props: ArrowProps): JSX.Element => {
-  return (
-    <div className="backArrow arrow" onClick={props.previousSlide}>
-      <i className="fa fa-arrow-left fa-2x" aria-hidden="true" />
-    </div>
-  );
-};
-
-/**
- * Represents the left arrow overlayed on the current slide.
- * @param props.nextSlide the function to get the previous slide.
- * @returns JSX element representing a clickable left arrow.
- */
-const RightArrow = (props: ArrowProps): JSX.Element => {
-  return (
-    <div className="nextArrow arrow" onClick={props.nextSlide}>
-      <i className="fa fa-arrow-right fa-2x" aria-hidden="true" />
-    </div>
+    <IonSlides pager={false} options={slideOpts}>
+      {images.map((image: string, i: number) => {
+        return createSlide(image, i);
+      })}
+    </IonSlides>
   );
 };
 
