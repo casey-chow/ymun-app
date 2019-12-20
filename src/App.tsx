46101a2f1e198347a94d1c85ed9f5c784de935ba
Casey Chow <casey.chow@gmail.com>
2019-12-20 02:32:26 -0800

    remove unused sections

--- src/App.tsx
+++ src/App.tsx
@@ -22,22 +22,18 @@ import '@ionic/react/css/structure.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/typography.css';
-import { book, calendar, send, time } from 'ionicons/icons';
+import { book, calendar, time } from 'ionicons/icons';
 import React, { Suspense } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { CacheProvider, NetworkErrorBoundary } from 'rest-hooks';
-import Details from './pages/Details';
 import EventDetail from './pages/EventDetail';
 import EventList from './pages/EventList';
-import Posts from './pages/Posts';
 import PostDetail from './pages/PostDetail';
+import Posts from './pages/Posts';
+import Press from './pages/PressHome/index';
 import ResourceCategoryDetail from './pages/ResourceCategoryDetail/index';
 import ResourceCategoryList from './pages/ResourceCategoryList/index';
 import ResourcePageDetail from './pages/ResourcePageDetail/index';
-import Press from './pages/PressHome/index';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 /* Theme variables */
 import './theme/variables.css';
 
@@ -67,10 +63,6 @@ const App: React.FC = () => (
                   path="/resources/:id"
                   component={ResourceCategoryDetail}
                 />
-                <Route path="/tab1" component={Tab1} exact />
-                <Route path="/tab2" component={Tab2} exact />
-                <Route path="/tab2/details" component={Details} />
-                <Route path="/tab3" component={Tab3} />
                 <Route
                   path="/"
                   render={() => <Redirect to="/press" />}
@@ -89,10 +81,6 @@ const App: React.FC = () => (
                 <IonTabButton tab="resources" href="/resources">
                   <IonIcon icon={book} />
                   <IonLabel>Resources</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="tab3" href="/tab3">
-                  <IonIcon icon={send} />
-                  <IonLabel>Tab Three</IonLabel>
                 </IonTabButton>
               </IonTabBar>
             </IonTabs>
