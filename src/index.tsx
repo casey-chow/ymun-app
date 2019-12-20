3d9eb5d3455c4e693e6182f892a123c3fb684dd5
Casey Chow <casey.chow@gmail.com>
2019-12-20 00:52:06 -0800

    splash screen ending

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
+import { Plugins } from '@capacitor/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const { SplashScreen } = Plugins;
+
+// https://capacitor.ionicframework.com/docs/apis/splash-screen/
+SplashScreen.hide();
 
 ReactDOM.render(<App />, document.getElementById('root'));
