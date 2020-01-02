f3e909e4e1fa6b4bc6eda9520ec7c0fd1a6c81d4
Casey Chow <casey.chow@gmail.com>
2020-01-01 23:17:00 -0500

    fix build

--- android/app/src/main/java/org/caseychow/ymunapp/MainActivity.java
+++ android/app/src/main/java/org/caseychow/ymunapp/MainActivity.java
@@ -1,9 +1,6 @@
 package org.caseychow.ymunapp;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.widget.Toolbar;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -11,15 +8,6 @@ import com.getcapacitor.Plugin;
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
-//  private Toolbar toolbar;
-//  private TabLayout tabLayout;
-//  private ViewPager viewPager;
-//  private int[] tabIcons = {
-//          R.drawable.ic_,
-//          R.drawable.ic_tab_call,
-//          R.drawable.ic_tab_contacts
-//  };
-
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
